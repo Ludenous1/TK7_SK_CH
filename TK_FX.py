3cb09fe6e864a92b3b6522293c0d6148e092f397
Ludenous <99399209+Ludenous1@users.noreply.github.com>
2023-07-09 20:57:25 -0400

    Update TK_FX.py

--- TK_FX.py
+++ TK_FX.py
@@ -114,8 +114,8 @@ def TekkenFBXexporter():
     elif Result == "PSK":
         bpy.ops.export_scene.fbx(filepath=File_Path, check_existing=True, filter_glob='*.fbx', use_selection=True, use_active_collection=False, global_scale=1.0, apply_unit_scale=True, apply_scale_options='FBX_SCALE_NONE', bake_space_transform=False, object_types={'ARMATURE', 'CAMERA', 'EMPTY', 'LIGHT', 'MESH', 'OTHER'}, use_mesh_modifiers=True, use_mesh_modifiers_render=True, mesh_smooth_type='FACE', use_subsurf=False, use_mesh_edges=False, use_tspace=True, use_custom_props=False, add_leaf_bones=False, primary_bone_axis='Y', secondary_bone_axis='X', use_armature_deform_only=True, armature_nodetype='NULL', bake_anim=False, bake_anim_use_all_bones=True, bake_anim_use_nla_strips=True, bake_anim_use_all_actions=True, bake_anim_force_startend_keying=True, bake_anim_step=1.0, bake_anim_simplify_factor=1.0, path_mode='AUTO', embed_textures=False, batch_mode='OFF', use_batch_own_dir=True, use_metadata=True, axis_forward='-Z', axis_up='Y')
 
-    if FBX_script_test == False:
-        Armature_Object_Restore(SK, SK_Og_Name)  
+    # if FBX_script_test == False:
+    #     Armature_Object_Restore(SK, SK_Og_Name)  
 
 def ApplyPose():
     
